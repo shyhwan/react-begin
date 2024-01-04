e92726195e703ca43c116dc60aa75ff01a8960f0
YSH-RRS <shallwenjoy@gmail.com>
2024-01-04 23:30:11 +0900

    Updates

---
+++ precache-manifest.e418eea298178a78810e69c7cd191d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f5f820196eab60a9650b463a621b1b",
+    "url": "/react-begin/index.html"
+  },
+  {
+    "revision": "6840b4871b5790d92116",
+    "url": "/react-begin/static/js/2.af0b0980.chunk.js"
+  },
+  {
+    "revision": "c9c5f12326e730f6171c9e7086115877",
+    "url": "/react-begin/static/js/2.af0b0980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b03d6999cd4e1f3adbc",
+    "url": "/react-begin/static/js/main.1d56e11e.chunk.js"
+  },
+  {
+    "revision": "373b4dc74efa993d1b28",
+    "url": "/react-begin/static/js/runtime-main.f8c4c4b8.js"
+  }
+]);
